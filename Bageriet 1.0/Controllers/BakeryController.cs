abbdea8ad523eebd59c6897dc4ff9e22d91666c7
Stefan Gillsbro <stefan.gillsbro@gmail.com>
2019-12-12 23:41:43 +0100

    Added contact information

---
+++ Bageriet 1.0/Controllers/BakeryController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using Bageriet_1._0.Models;
+
+namespace Bageriet_1._0.Controllers
+{
+    public class BakeryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Greeting"] = "Hello";
+            ViewData["Address"] = new Address()
+            {
+                Name = "Campus Värnamo",
+                Street = "Jönköpingsvägen 15",
+                City = "Värnamo",
+                PostalCode = "331 34"
+            };
+
+            return View();
+        }
+
+        // GET: /Bakery/Welcome/ 
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
--- Bageriet 1.0/Startup.cs
+++ Bageriet 1.0/Startup.cs
@@ -35,7 +35,7 @@ namespace Bageriet_1._0
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/BAkery/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace Bageriet_1._0
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Bakery}/{action=Index}");
             });
         }
     }
